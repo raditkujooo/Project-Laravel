d1939d46b3ab05b8b60a911dbe4330d680c733fa
RadityaFadhlanPratama <raditkujo123@gmail.com>
2024-06-13 19:18:50 +0700

    project-laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <a href="#" class="logo">portofo<span>lio</span></a>
+
+        <nav class="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#about">About</a>
+            <a href="#skils">Skill</a>
+            
+        </nav>
+    </header>
+
+    <section class="home" id="home">
+        <div class="home-content">
+            <h1>Hi, I'm Raditya Fadhlan Pratama</h1>
+            <h3>im wikrama's student</h3>
+            <div class="btn-box">
+                <a href="#">Hello</a>
+                <a href="#">Code</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- design -->
+    <section class="about" id="about">
+        <h2 class="heading">About <span>Me</span></h2>
+        <span class="spin"></span>
+
+        <div class="about-foto">
+            <img src="assets/img/5yokujo.jpg" alt="profile">
+            <span class="cc-spin"></span>
+        </div>
+        <div class="about-content">
+            <h3>This is me!</h3>
+            <p>Hello y'all!! my name is Raditya Fadhlan Pratama
+                <br> I live in Bogor, I am 16 years old, I was born in Bogor on April 07 2008, 
+                i dont know much about code but, 
+                i will try my best!!! </br>I am a student of SMK WIKRAMA
+                BOGOR, I major in PPLG
+            </p>
+
+            <div class="btn-box btns">
+                <a href="#" class="btn">Read More</a>
+            </div>
+        </div>
+    </section>
+    <!-- skill design -->
+    <section class="skils" id="skils">
+        <h2 class="heading">My<span>Skills</span></h2>
+
+        <div class="skils-row">
+            <div class="skils-column">
+                <h3 class="title">Coding Skils</h3>
+                <div class="skils-box">
+                    <div class="skils-content">
+                        <div class="progress">
+                            <h3>HTML <span>51%</span></h3>
+                            <div class="bar"><span></span></div>
+
+                        </div>
+                        <div class="progress">
+                            <h3>CSS <span>37%</span></h3>
+                            <div class="bar"><span></span></div>
+
+                        </div>
+                        <div class="progress">
+                            <h3>Java Script <span>10% </span></h3>
+                            <div class="bar"><span></span></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- contact design -->
+    <section class="contact" id="contact">
+    
+</body>
+</html>
